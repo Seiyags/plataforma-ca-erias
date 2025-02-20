efaf426a90c56231e70613f0ccb0882556a9d524
Damian <damianperezherediaalejandro@gmail.com>
2025-02-20 16:28:34 -0300

    MVP

---
+++ Plataforma-Canerias/Plataforma-Canerias/src/pages/Descargar.jsx
@@ -0,0 +1,11 @@
+const Descargar = () => {
+    return (
+      <div>
+        <h2 className="text-3xl mb-4 font-bold">Descargar</h2>
+        <p>Contenido visual relacionado.</p>
+      </div>
+    );
+  };
+  
+  export default Descargar;
+  
--- Plataforma-Canerias/src/pages/Visual.jsx
+++ Plataforma-Canerias/src/pages/Visual.jsx
@@ -1,8 +1,8 @@
 const Visual = () => {
     return (
       <div>
-        <h2 className="text-3xl mb-4 font-bold">Página Visual</h2>
-        <p>Contenido visual relacionado.</p>
+        <h2 className="text-center text-xl pt-10">Página Visual</h2>
+        <p className="text-center text-xs px-30 py-5 text-gray-700">Contenido visual relacionado.</p>
       </div>
     );
   };
